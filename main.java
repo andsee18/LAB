ba4e912fd0f057a4f754e394bb4f2a0a53938df1
andsee18 <krllx6@mail.ru>
2024-09-22 20:50:07 +0300

    проект

---
+++ main.java
@@ -0,0 +1,25 @@
+package main;
+public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            String word = args[i];
+            boolean temp = isPalindrome(word);
+
+            System.out.println(temp);
+        }
+
+    }
+
+    public static String reverseString(String str) {
+        String s = "";
+        for (int i = str.length(); i > 0; i--) {
+            s += str.charAt(i - 1);
+        }
+        return s;
+    }
+
+    public static boolean isPalindrome(String str) {
+        String s2 = reverseString(str);
+        return s2.equals(str);
+    }
+}
